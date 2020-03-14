2ab4cb4f08fbb2aa3fb2351ed25f14471bba71b8
jakson.laquini <jakson.laquini@itixti.com.br>
2020-03-13 23:03:56 -0300

    debugger e enviar anexo email

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var cors = require('cors')
-var indexRouter = require('./routes/index');
 
 var app = express();
-
 app.use(cors())
+const fileUpload = require('express-fileupload');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
+// Parse json data
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var indexRouter = require('./routes/index');
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended:true}));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/email', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -22,18 +22,23 @@ var mailOptions = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //   }
+  // });
   res.json({teste: 'teste dois'});
 });
 
 /* Post home page. */
 router.post('/enviar', function(req, res, next) {
+  mailOptions.to = '';
+  mailOptions.subject = '';
+  mailOptions.html = '';
+  mailOptions.attachments = [];
+
   const nome =  req.body.nome;
   const telefone = req.body.telefone;
   const email = req.body.email;
@@ -52,17 +57,30 @@ router.post('/enviar', function(req, res, next) {
   });
 });
 
-router.post('/enviarOrcamento', function(req, res, next) {
+router.post('/enviarorcamento', function(req, res, next) {
+ 
+  mailOptions.to = '';
+  mailOptions.subject = '';
+  mailOptions.html = '';
+  mailOptions.attachments = [];
 
+  const servico = req.body.servico;
   const nome =  req.body.nome;
   const telefone = req.body.telefone;
   const email = req.body.email;
   const endereco = req.body.endereco;
   const mensagem = req.body.mensagem;
 
-  mailOptions.to = 'contato@geramaisengenharia.com.br';
+  mailOptions.to = 'orcamento@geramaisengenharia.com.br';
   mailOptions.subject = 'Orçamento';
-  mailOptions.html = '<h1>Solicitação de Orçamento</h1><br><p><b>NOME:  </b> ' + nome + ' </p><p><b>TELEFONE:  </b> ' + telefone + ' </p><p><b>EMAIL: </b> ' + email + ' </p><p><b>ENDEREÇO: </b> ' + endereco + ' </p><p><b>MENSAGEM:  </b>' + mensagem + ' </p>';
+  mailOptions.html = '<h1>Solicitação de Orçamento</h1><br><p><b>SERVIÇO PRETENDIDO:  </b> ' + servico + ' </p><p><b>Nome:  </b> '+ nome + ' </p><p><b>TELEFONE:  </b> ' + telefone + ' </p><p><b>EMAIL: </b> ' + email + ' </p><p><b>ENDEREÇO: </b> ' + endereco + ' </p><p><b>MENSAGEM:  </b>' + mensagem + ' </p>';
+  
+  if (req.files) {
+    mailOptions.attachments = [{ 
+      filename: req.files.anexo.name, 
+      content: new Buffer(req.files.anexo.data,'utf-8')
+    }]
+  }
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
